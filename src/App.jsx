53ed1e194c50d58a66461d56dfdd972292e305ad
iamlydial <lydia.lavecchia@gmail.com>
2024-11-13 13:43:48 +0100

    chore: set up project structure with src and public folders, added App.jsx and index.js as entry point

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>Hello, React with SASS (using .sass files)!</h1>
+    </div>
+  );
+};
+
+export default App;
