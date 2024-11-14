93510d6ac829b9327722aa2930340bc60073921f
iamlydial <lydia.lavecchia@gmail.com>
2024-11-14 10:53:53 +0100

    feat: att .env file to fetch url and key and add function to fetch movies by category and by id

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,14 +1,15 @@
-const API_KEY = "your_movie_api_key";
-const BASE_URL = "https://api.themoviedb.org/3";
-
 export const fetchMoviesByCategory = async (category) => {
-  const response = await fetch(`${BASE_URL}/movie/${category}?api_key=${API_KEY}`);
+  const response = await fetch(
+    `${import.meta.BASE_URL}/movie/${category}?api_key=${import.meta.API_KEY}`
+  );
   const data = await response.json();
   return data.results;
 };
 
 export const fetchMovieById = async (id) => {
-  const response = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+  const response = await fetch(
+    `${import.meta.BASE_URL}/movie/${id}?api_key=${import.meta.API_KEY}`
+  );
   const data = await response.json();
   return data;
 };
