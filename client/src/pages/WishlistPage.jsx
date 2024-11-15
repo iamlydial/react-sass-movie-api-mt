7ef5ae7d8233a6d4e84f33594cef4d918ce705e3
iamlydial <lydia.lavecchia@gmail.com>
2024-11-15 19:02:07 +0100

    feat add redux and folder structure for the wishlist slice

--- client/src/pages/WishlistPage.jsx
+++ client/src/pages/WishlistPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const WishlistPage = () => {
-  return <div>WishlistPage</div>;
+  return <div>
+    <h1>Wishlist</h1>
+    <p>Here are the movies you've saved for later. 🍿</p>
+    <div>
+      {/* Display the movies in the wishlist */}
+      
+    </div>
+  </div>;
 };
 
 export default WishlistPage;
