7bbf0182272d6e68e3422666a0f5b0b8d702f0ee
iamlydial <lydia.lavecchia@gmail.com>
2024-11-15 18:40:06 +0100

    feat complete detail page layout
    - adjust webpack with react hot reloader

--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -1,35 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchMovieById } from "../utils/api";
+import "../styles/pages/detail-page.sass";
 
 const DetailPage = ({ addToWishlist }) => {
   const { id } = useParams();
-  console.log(id, "id");
   const [movie, setMovie] = useState(null);
 
   useEffect(() => {
     fetchMovieById(id)
-      .then(setMovie)
+      .then((data) => {
+        console.log(data); // Log the fetched data
+        setMovie(data);
+      })
       .catch((error) => console.error(error));
   }, [id]);
 
-  console.log("movie", movie);
-
   if (!movie) return <div>Loading...</div>;
+
+  console.log(movie.poster_path, "movie.imageUrl");
 
   return (
     <div className="detail-page">
-      <div className="img-side">
-        <img
-          className="detail-img"
-          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          alt={movie.title}
-        />
+      <div className="content">
+        <div className="img-side">
+          {movie.poster_path && (
+            <img
+              className="detail-img"
+              src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+              alt={movie.title}
+            />
+          )}
+        </div>
+        <div className="info-side">
+          <h1>{movie.title}</h1>
+          <p>{movie.overview}</p>
+          <button onClick={() => addToWishlist(movie)}>ADD TO WISHLIST</button>
+        </div>
       </div>
-      <div className="info-side">
-        <h1>{movie.title}</h1>
-        <p>{movie.overview}</p>
-        <button onClick={() => addToWishlist(movie)}>Add to Wishlist</button>
+      <div className="additional-info">
+        <h2>Details</h2>
+        <p>Release Date: {movie.release_date}</p>
+        <p>Rating: {movie.vote_average}</p>
+        <p>Runtime: {movie.runtime} minutes</p>
       </div>
     </div>
   );
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 import dotenv from "dotenv";
 import webpack from "webpack";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'; 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,6 +17,7 @@ export default {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -26,11 +28,12 @@ export default {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ['react-refresh/babel'],
           },
         },
       },
       {
-        test: /\.sass$/,
+        test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
@@ -46,5 +49,15 @@ export default {
     new webpack.DefinePlugin({
       "process.env": JSON.stringify(process.env),
     }),
+    new ReactRefreshWebpackPlugin(), 
   ],
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'dist'), 
+    },
+    compress: true,
+    port: 8080,
+  },
 };
